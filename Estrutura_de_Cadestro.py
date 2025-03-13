c2aea88281c6e59d74138ed2404344e4b00a62ed
Gustavo Gomes Gonçalvez <gugomesjv@gmail.com>
2025-03-13 19:42:32 -0300

    Update Estrutura_de_Cadestro.py

--- Estrutura_de_Cadestro.py
+++ Estrutura_de_Cadestro.py
@@ -1,10 +1,10 @@
 # listas
 
 cpf = [12345]
-Nome =['guatvo']
+nome =['guatvo']
 sexo = ['m']
 bairro = ['xery']
-Responsaveis = ['jdj']
+responsaveis = ['jdj']
 
 loop = 's'
 
@@ -25,7 +25,7 @@ while loop == 's':
             cpf.append(int(input('Digite o CPF do aluno: ')))
             sexo.append(input('Digite o sexo do aluno: '))
             bairro.append(input('Digite o bairro do aluno: '))
-            Responsaveis.append(input('Digite o nome do responsaveris: '))
+            responsaveis.append(input('Digite o nome do responsaveris: '))
             print('')
 
     elif opcao == 2:
@@ -33,23 +33,29 @@ while loop == 's':
         print('Digite o CPF do aluno')
         alterar = int(input("Digite qual deseja alterar: "))
         if alterar in (cpf):
+
             Nome[alterar] = input("Digite o novo nome: ")
             sexo[alterar] = input("Digite o novo sexo: ")
             bairro[alterar] = input("Digite o novo bairro: ")
-            Responsaveis[alterar] = input("Digite o novo responsável: ")
+            responsaveis[alterar] = input("Digite o novo responsável: ")
         else:
          print("alteraçâo inválido.")
 
     elif opcao == 3:
         #Função Deletar
-
         vdel = int(input('Qual elemento apagar?'))
-        del cpf[vdel]
-        del nome [vdel]
-        del sexo[vdel]
-        del bairro[vdel]
-        del responsaveis[vdel]
-        print(cpf)
+
+        if vdel in cpf:
+
+            posicao = cpf.index(vdel)
+            
+            del cpf[posicao]
+            del nome[posicao]
+            del sexo[posicao]
+            del bairro[posicao]
+            del responsaveis[posicao]
+            print(cpf)
+
 
     elif opcao == 4:
         print("Saindo do programa...")
